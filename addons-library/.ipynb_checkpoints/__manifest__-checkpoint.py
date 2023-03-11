a3d7682f2c51c71354fa859ab53361c7dccc30f2
raldlucas <renan@proyectasoft.com>
2023-03-11 20:44:04 +0000

    [ADD]Library

---
+++ addons-library/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Library ',
+    'summary': """ Library Manager""",
+
+    'description': """
+        Module to manage a library
+        """,
+
+    'licence': 'OPL-1',
+
+    'author': 'Odoo',
+
+    'website': 'https://www.odoo.com',
+
+    'category': 'Trainingg',
+
+    'version': '0.1',
+
+    'depends': ['base'],
+
+    'data': [
+        #'security/book_security.xml',
+
+    ],
+
+    'demo': [
+
+    ],
+
+}
